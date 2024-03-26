be9467eca04539c52110372356d8dac5ad5b0f82
Thna17 <hangbrathna10@gmail.com>
2024-03-26 10:25:29 +0700

    Algorithm python: sum of all natural number

---
+++ solve.1.py
@@ -0,0 +1,8 @@
+#Algorithm 1.1
+def solve(N):
+    s = 0
+    for i in range(1, N + 1):
+        s += i
+    return s
+N = int(input())
+print(solve(N))
---
+++ solve.2.py
@@ -0,0 +1,6 @@
+#Algorithm 1.2
+def solve(n):
+    return n * (n + 1) // 2
+N = int(input())
+print(solve(N))
+
